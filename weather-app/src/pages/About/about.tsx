e0a32cbc90acd0df624458634abdb4ca2478bc95
Ivan <dropdeaddropdead@yandex.ru>
2024-07-23 18:18:29 +0300

    feat: mui-icons

--- weather-app/src/pages/About/about.tsx
+++ weather-app/src/pages/About/about.tsx
@@ -1,3 +1,5 @@
+import TelegramIcon from '@mui/icons-material/Telegram';
+
 function About() {
   return (
     <>
@@ -15,12 +17,15 @@ function About() {
           онлайн-магазина
         </a>{' '}
         (но, к сожалению, сейчас его нормально не потестить, т.к ключ апи был
-        всего на месяц). Делал и многостраничные проекты и SPA. Понимаю основные
+        всего на месяц). Делал многостраничные проекты и SPA. Понимаю основные
         принципы создания и проектирования веб-приложений. <br></br> На данный
         момент продолжаю изучать React. На данный момент владею: JavaScript,
         HTML5, CSS3, Vite, WebPack, SASS/SCSS, GIT, TypeScript, React, Axios,
         TanstackQuery, Postman. Soft-skills: коммуникативность, критическое
-        мышление, стрессоустойчивость. telegram: @drpdd
+        мышление, стрессоустойчивость. contact me:
+        <a href='https://t.me/drpdd' target='blank'>
+          <TelegramIcon></TelegramIcon>@drpdd
+        </a>
       </div>
     </>
   );
